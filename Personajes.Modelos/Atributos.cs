c411379f1aa19ace04ab5900a2ef4014a3eaf393
Un enano mas <whatdoyoucare@outlook.com>
2019-08-21 23:16:38 -0500

    Initial Load

---
+++ Personajes.Modelos/Atributos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Personajes.Modelos
+{
+    public class Atributos
+    {
+
+
+
+        public Atributos() {
+
+        }
+    }
+}
